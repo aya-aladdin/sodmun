74b54f11e4ba19ceb5ac0d4b6259ed52e0b59abc
siddharthajith2009 <84459607+siddharthajith2009@users.noreply.github.com>
2025-09-15 20:34:49 +0400

    Update website templates and add partner image

--- sodmun/app.py
+++ sodmun/app.py
@@ -123,4 +123,4 @@ def page_not_found(error):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
